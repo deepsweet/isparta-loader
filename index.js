8910fa3099527c27df263b67a4933db2391c6ee3
deepsweet <kir@soulshine.in>
2015-09-18 14:19:23 +0700

    :heavy_minus_sign: remove loader options

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 'use strict';
 
 var isparta = require('isparta');
-var loaderUtils = require('loader-utils');
-var assign = require('lodash.assign');
 
 module.exports = function(source) {
+    var instrumenter = new isparta.Instrumenter({
+        embedSource: true,
+        noAutoWrap: true
+    });
+
     if (this.cacheable) {
         this.cacheable();
     }
-
-    var options = loaderUtils.parseQuery(this.query);
-    var instrumenter = new isparta.Instrumenter(assign(
-        {
-            embedSource: true,
-            noAutoWrap: true
-        },
-        options
-    ));
 
     return instrumenter.instrumentSync(source, this.resourcePath);
 };
