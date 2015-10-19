ea847551b1a5af3fc43211aafe2d1ab4d88582cf
Vyacheslav Slinko <vslinko@yahoo.com>
2015-10-19 12:58:59 +0300

    Read isparta config from webpack confg

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var isparta = require('isparta');
 
 module.exports = function(source) {
-    var instrumenter = new isparta.Instrumenter({
+    var config = this.options.isparta || {
         embedSource: true,
         noAutoWrap: true,
         babel: this.options.babel
-    });
+    };
+
+    var instrumenter = new isparta.Instrumenter(config);
 
     if (this.cacheable) {
         this.cacheable();
